9a548b7d833ab105d412f775fd2265a8a28e2f32
fighter3 <yangqinjunbj@qq.com>
2021-01-17 14:56:43 +0800

    📕登录界面

--- demo-vue/src/router/index.js
+++ demo-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+//导入登录页面组件
+import Login from '@/views/login.vue'
 
 Vue.use(Router)
 
@@ -10,6 +12,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    //添加登录页面路由
+    {
+      path:'/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
