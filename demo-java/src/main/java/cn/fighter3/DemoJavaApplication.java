8dfc66534f51bbdbf97f43116148f1360617bb9f
fighter3 <yangqinjunbj@qq.com>
2021-01-17 13:59:50 +0800

    ➕ 集成MP完成增删改查

--- demo-java/src/main/java/cn/fighter3/DemoJavaApplication.java
+++ demo-java/src/main/java/cn/fighter3/DemoJavaApplication.java
@@ -1,9 +1,11 @@
 package cn.fighter3;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.fighter3.mapper")
 public class DemoJavaApplication {
 
     public static void main(String[] args) {
---
+++ demo-java/src/main/java/cn/fighter3/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.fighter3.mapper;
+
+import cn.fighter3.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Author: 三分恶
+ * @Date: 2021/1/17
+ * @Description: TODO
+ **/
+
+public interface UserMapper extends BaseMapper<User> {
+}
