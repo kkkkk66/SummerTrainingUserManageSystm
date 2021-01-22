2278ad7169ee900693482b1100655b3eb11df985
fighter3 <hpuyangqinjun@foxmail.com>
2021-01-22 15:10:28 +0800

    🔨 登录状态保持

--- demo-vue/src/components/HelloWorld.vue
+++ demo-vue/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="demo">
-    {{msg}}
+    {{userName}}
   </div>
 </template>
 
@@ -10,6 +10,11 @@ export default {
   data () {
     return {
       msg: 'Hello Vue!'
+    }
+  },
+  computed: {
+    userName() {
+      return this.$store.state.user.userName
     }
   }
 }
--- demo-vue/src/store/index.js
+++ demo-vue/src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        token: '',
+        token: sessionStorage.getItem("token"),
         user: JSON.parse(sessionStorage.getItem("user"))
     },
     mutations: {
